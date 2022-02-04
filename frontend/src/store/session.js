ff47c12f5953153954536eded840b88284034e0a
daniel <danielthai14@gmail.com>
2022-02-04 13:38:16 -0700

    signup form working

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -36,6 +36,18 @@ export const restoreUser = () => async (dispatch) => {
   return response;
 };
 
+//signup thunk
+export const signup =(user) => async dispatch => {
+  const {username, email, password} = user;
+  const response = await csrfFetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({ username, email, password })
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
