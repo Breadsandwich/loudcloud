e9610ffb2bc1b064050c28bc9f31b913f870edc1
daniel <danielthai14@gmail.com>
2022-02-04 14:55:47 -0700

    basic navigation set up

---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
+
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <ProfileButton user={sessionUser} />
+    );
+  } else {
+    sessionLinks = (
+      <>
+        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </>
+    );
+  }
+
+  return (
+    <nav className='navbar'>
+      <div className='navbar-box'>
+        <NavLink exact to="/">Home</NavLink>
+        {isLoaded && sessionLinks}
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
