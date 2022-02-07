aad8051cd1670dc5c742e92c6493e28152fb6b97
daniel <danielthai14@gmail.com>
2022-02-07 12:11:56 -0700

    logging in changes splashpage to mainpage

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -4,7 +4,9 @@ import './MainPage.css'
 const MainPage = () => {
     return (
         <div className="main_content_container">
-
+            <ul className='songList_item'>
+                
+            </ul>
         </div>
     )
 }
